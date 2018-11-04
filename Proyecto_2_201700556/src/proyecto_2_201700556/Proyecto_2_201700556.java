6f12a7bff320273ac8fa08d088e6ce60a1f45360
achiv1755 <juanquis1117@gmail.com>
2018-11-04 09:50:55 -0600

    comit 7

---
+++ Proyecto_2_201700556/src/proyecto_2_201700556/Proyecto_2_201700556.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyecto_2_201700556;
+import proyecto_2_201700556.dinamico.Administrador;
+import proyecto_2_201700556.dinamico.Login;
+/**
+ *
+ * @author juare
+ */
+public class Proyecto_2_201700556 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    Administrador a=new Administrador();
+    a.add();
+//Login login=new Login();
+      // login.buscar();
+    }
+    
+}
